975687259e465ab224bb3f5c3f3bd16b6bcd6fdb
SectorNN <net.laifa@gmail.com>
2022-09-02 22:19:58 +0300

    bb

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите элементы массива через запятую: ");
+string [] arr = Console.ReadLine().Split(",");
+List<int> vals = new List<int>();
+
+for (int i = 0; i < arr.Length; i++){
+    if (int.Parse(arr[i]) % 2 == 0)
+    {
+        vals.Add(int.Parse(arr[i]));
+    }
+}
+
+Console.Write(string.Join(" ", vals));
