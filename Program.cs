5419902ae673879cf7e5adb0f4f9bf5e1404700e
SectorNN <net.laifa@gmail.com>
2022-09-02 21:52:29 +0300

    m

--- Program.cs
+++ Program.cs
@@ -1,24 +1,12 @@
-﻿/*
-Написать программу, которая на вход получает массив данных от пользователя через запятую и на выходе выдаёт массив состоящий из четных чисел
-пример:
-1,2,3,4,5,6,7,8 => 2 4 6 8
-*/
-string str = Console.ReadLine();
-char[] charArr = new char[str.Length];
-charArr = str.ToCharArray();
-str = "";
-for (int i = 0; i < charArr.Length; i++)
-{
-    if (charArr[i] == ',')
-    {
-        continue;
-    }
-    if (charArr[i] % 2 == 0)
-    {
+﻿Console.WriteLine("Введите элементы массива через запятую: ");
+string [] arr = Console.ReadLine().Split(",");
+List<int> vals = new List<int>();
 
-        str += charArr[i];
+for (int i = 0; i < arr.Length; i++){
+    if (int.Parse(arr[i]) % 2 == 0)
+    {
+        vals.Add(int.Parse(arr[i]));
     }
 }
-char [] array = str.ToCharArray();
 
-Console.Write(string.Join(" ", array));
+Console.Write(string.Join(" ", vals));
